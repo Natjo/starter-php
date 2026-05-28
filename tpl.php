f48513ed4ebc09df51612296c786e6172984dd2c
Natjo <natjo@natjo.com>
2026-05-28 11:58:18 +0200

    wip

--- tpl.php
+++ tpl.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon projet</title>
     <link rel="icon" href="/dist/assets/favicon/favicon.ico" type="image/x-icon">
+    
     <link rel="stylesheet" href="/dist/assets/styles.css">
 </head>
 <body>
@@ -13,6 +14,6 @@
         <!-- Contenu principal -->
     </div>
 
-    <script src="/dist/assets/app.js"></script>
+    <script src="/dist/assets/app.js" defer type="module"></script>
 </body>
 </html>
